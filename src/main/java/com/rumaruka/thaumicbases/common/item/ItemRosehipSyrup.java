3090af6def1ac74b877dca18f3972c792ca31ce1
a <EYCHFSJD@gmail.com>
2024-09-06 14:26:03 +0800

    new Texures bug fix version bump

--- src/main/java/com/rumaruka/thaumicbases/common/item/ItemRosehipSyrup.java
+++ src/main/java/com/rumaruka/thaumicbases/common/item/ItemRosehipSyrup.java
@@ -17,10 +17,8 @@ import thaumcraft.api.items.ItemsTC;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ItemRosehipSyrup extends Item
-{
-    public ItemRosehipSyrup()
-    {
+public class ItemRosehipSyrup extends Item {
+    public ItemRosehipSyrup() {
         this.setMaxStackSize(16);
     }
 
@@ -28,53 +26,40 @@ public class ItemRosehipSyrup extends Item
      * Called when the player finishes using this Item (E.g. finishes eating.). Not called when the player stops using
      * the Item before the action is complete.
      */
-    public ItemStack onItemUseFinish(ItemStack stack, World worldIn, EntityLivingBase entityLiving)
-    {
-        if (!worldIn.isRemote) entityLiving.curePotionEffects(stack); // FORGE - move up so stack.shrink does not turn stack into air
-        if (entityLiving instanceof EntityPlayer)
-        {
+    public ItemStack onItemUseFinish(ItemStack stack, World worldIn, EntityLivingBase entityLiving) {
+        if (!worldIn.isRemote)
+            entityLiving.curePotionEffects(stack); // FORGE - move up so stack.shrink does not turn stack into air
+        if (entityLiving instanceof EntityPlayer) {
             this.removeNegativeEffect(entityLiving);
-            ThaumcraftApi.internalMethods.addWarpToPlayer((EntityPlayer) entityLiving,-4,IPlayerWarp.EnumWarpType.TEMPORARY);
-
+            ThaumcraftApi.internalMethods.addWarpToPlayer((EntityPlayer) entityLiving, -4, IPlayerWarp.EnumWarpType.TEMPORARY);
 
 
         }
 
-        if (entityLiving instanceof EntityPlayer && !((EntityPlayer)entityLiving).capabilities.isCreativeMode)
-        {
+        if (entityLiving instanceof EntityPlayer && !((EntityPlayer) entityLiving).capabilities.isCreativeMode) {
             stack.shrink(1);
         }
 
         return stack.isEmpty() ? new ItemStack(ItemsTC.phial) : stack;
     }
-    public static void removeNegativeEffect(EntityLivingBase entity)
-    {
+
+    public static void removeNegativeEffect(EntityLivingBase entity) {
         List<Potion> potions = new ArrayList<>();
         potions.addAll(entity.getActivePotionMap().keySet());
         potions.stream().filter(potion -> potion.isBadEffect()).forEach(entity::removeActivePotionEffect);
     }
 
 
-
-    public int getMaxItemUseDuration(ItemStack stack)
-    {
+    public int getMaxItemUseDuration(ItemStack stack) {
         return 32;
     }
 
 
-    public EnumAction getItemUseAction(ItemStack stack)
-    {
+    public EnumAction getItemUseAction(ItemStack stack) {
         return EnumAction.DRINK;
     }
 
-    @Override
-    public net.minecraftforge.common.capabilities.ICapabilityProvider initCapabilities(ItemStack stack, net.minecraft.nbt.NBTTagCompound nbt) {
-        return new net.minecraftforge.fluids.capability.wrappers.FluidBucketWrapper(stack);
-    }
-
-
-    public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn)
-    {
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
         playerIn.setActiveHand(handIn);
         return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, playerIn.getHeldItem(handIn));
     }

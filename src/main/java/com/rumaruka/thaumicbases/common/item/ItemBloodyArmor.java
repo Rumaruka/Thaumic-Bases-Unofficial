887a43cd9faa680859456b203e36fcd70dc86067
a <EYCHFSJD@gmail.com>
2025-09-05 22:06:56 +0800

    acatually fixed.

--- src/main/java/com/rumaruka/thaumicbases/common/item/ItemBloodyArmor.java
+++ src/main/java/com/rumaruka/thaumicbases/common/item/ItemBloodyArmor.java
@@ -48,7 +48,7 @@ public class ItemBloodyArmor extends ItemArmor implements IVisDiscountGear {
 
     @Override
     public boolean getIsRepairable(ItemStack toRepair, ItemStack repair) {
-        return super.getIsRepairable(toRepair, repair) || toRepair.equals(new ItemStack(TBItems.bloodycloth, 1, 0));
+        return super.getIsRepairable(toRepair, repair) || toRepair.getItem() == TBItems.bloodycloth;
     }
 
     @Override
